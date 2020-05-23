6c5c20a5552719e8b8f020f55abbc8b1122b0efa
Rafael Castro Gomez <rafael.castro@atos.net>
2020-05-23 17:25:10 +0200

    Versión WS_SECURITY

---
+++ src/app/webservices/security/PWCallback.java
@@ -0,0 +1,29 @@
+package app.webservices.security;
+
+import java.io.IOException;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import org.apache.ws.security.WSPasswordCallback;
+
+public class PWCallback implements CallbackHandler {
+	
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+
+		for (Callback callback : callbacks) {	
+    	
+      if (callback instanceof WSPasswordCallback) {
+        WSPasswordCallback pc = (WSPasswordCallback)callback; 
+        String username = pc.getIdentifier();
+        String password = pc.getPassword();
+        if(!(username.equals("rcastro") && 
+        		 password.equals("rcastro"))) {
+        	throw new UnsupportedCallbackException(callback, "Credenciales de acceso incorrectas");	
+        }
+      } else {
+        throw new UnsupportedCallbackException(callback, "Unrecognized Callback");
+      }
+    }   
+	}
+	
+}
